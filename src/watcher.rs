f5e90dac4c1037fcb4be45539eff08bf2e2db9b2
Michal Vanko <mklakakilli@gmail.com>
2024-11-13 15:27:04 +0100

    first iteration on the watcher

--- src/watcher.rs
+++ src/watcher.rs
@@ -1,20 +1,24 @@
 use crate::config::Configuration;
 use log::{debug, info};
+use notify::RecursiveMode;
+use notify_debouncer_full::{new_debouncer, DebounceEventResult, DebouncedEvent};
 use std::{
     collections::HashMap,
-    path::{Path, PathBuf},
-    sync::{Arc, Mutex},
+    path::PathBuf,
+    sync::Arc,
     time::{Duration, Instant},
 };
-use tokio::time::sleep;
+use tokio::{sync::Mutex, time::sleep};
 
 pub struct RepositoryState {
+    path: PathBuf,
     last_change_at: Instant,
 }
 
-impl Default for RepositoryState {
-    fn default() -> Self {
+impl RepositoryState {
+    fn new(path: PathBuf) -> Self {
         Self {
+            path,
             last_change_at: Instant::now(),
         }
     }
@@ -27,25 +31,58 @@ pub async fn start_watcher() -> Result<(), anyhow::Error> {
 
     let initial_state: HashMap<String, RepositoryState> =
         HashMap::from_iter(cfg.repositories.iter().map(|repo| {
+            let path = repo.path.clone();
             (
-                repo.path.to_str().unwrap().to_string(),
-                RepositoryState::default(),
+                path.to_str().unwrap().to_string(),
+                RepositoryState::new(path),
             )
         }));
 
     let watch_state = Arc::new(Mutex::new(initial_state));
+    // What should I do here?
+    // I want to loop over the repositories but the repositories have to be always updated when anything happens.
+    // Whenever a repository is added/removed from the config we should store the state and restart?
+    // I will need to send the signal to the watcher that it should reload
+
+    let state_clone = watch_state.clone();
+    let repositories = state_clone.lock().await;
+
+    repositories.values().for_each(|repo_state| {
+        watch_repo(repo_state);
+    });
 
     // Main application loop
     info!("Starting git_afk to watch repositories");
     loop {
         // Do some work
-        check_repositories().await;
 
         // Sleep for 5 seconds
         sleep(Duration::from_secs(5)).await;
     }
 }
 
-async fn check_repositories() {
+fn watch_repo(repo_state: &RepositoryState) {
     debug!("Starting check for changes!");
+
+    let mut debouncer = new_debouncer(
+        Duration::from_secs(2),
+        None,
+        |watch_result: DebounceEventResult| match watch_result {
+            Ok(events) => events
+                .into_iter()
+                .for_each(|event| handle_watch_event(&event)),
+            Err(errors) => errors
+                .iter()
+                .for_each(|error| println!("notify error {error:?}")),
+        },
+    )
+    .unwrap();
+
+    debouncer
+        .watch(&repo_state.path, RecursiveMode::Recursive)
+        .unwrap();
+}
+
+fn handle_watch_event(event: &DebouncedEvent) {
+    debug!("We have a notify event {:?}", event);
 }
